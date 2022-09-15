dcc39fcfebceb8032abefadbb1840299c0a7c339
Bill Denney <wdenney@humanpredictions.com>
2022-09-15 14:34:11 -0400

    Simplify loading models

--- R/000-pkgsetup.R
+++ R/000-pkgsetup.R
@@ -23,7 +23,7 @@ addFileToModelDb <- function(dir, file) {
   # Extract the description from the first line of the file
   desc <- readLines(con = fileName, n = 1)
   descClean <- gsub(x = desc, pattern = "^# *Description: *", replacement = "")
-  
+
   # Extract the model from the file
   parsedFile <- parse(file = fileName)
   stopifnot(identical(parsedFile[[1]][[1]], as.name("<-")))
@@ -36,26 +36,37 @@ addFileToModelDb <- function(dir, file) {
 
   # Parse the model to get the fixed effects and DV parameters
   mod <- nlmixr2::nlmixr2(eval(parsedFile[[1]][[3]]))
-  
+
+  description <- mod$meta$description
+  if (is.null(description)) {
+    message("No description for model in ", fileName)
+    description <- NA_character_
+  }
+
   # Extract the parameter names
   modParam <- mod$iniDf
   # Fixed effects
-  modParamFixed <- modParam[is.na(modParam$neta1) & is.na(modParam$err), ]
+  modParamFixed <- modParam$name[is.na(modParam$neta1) & is.na(modParam$err)]
+
+  # swap modeled parameter names for the mu-ref parameter names, where
+  # applicable
+  for (nm in names(mod$getSplitMuModel$pureMuRef)) {
+    modParamFixed[modParamFixed %in% nm] <- mod$getSplitMuModel$pureMuRef[[nm]]
+  }
+
   # Error model
-  modParamErr <- modParam[!is.na(modParam$err), ]
-  stopifnot(nrow(modParamErr) == 1)
-  if (modParamErr$condition == "rxLinCmt") {
-    paramErr <- "linCmt()"
-  } else {
-    paramErr <- modParamErr$condition
+  paramErr <- mod$predDf$cond
+  if ("rxLinCmt" %in% paramErr) {
+    paramErr[paramErr %in% "rxLinCmt"] <- "linCmt()"
   }
 
   ret <-
     data.frame(
       name=modelName,
-      description=descClean,
-      Parameters=paste(modParamFixed$name, collapse = ","),
-      DV=paramErr
+      description=description,
+      Parameters=paste(modParamFixed, collapse = ","),
+      DV=paramErr,
+      filename=fileName
     )
   modeldb <<- rbind(modeldb, ret)
   if (any(duplicated(modeldb$name))) {
