e0ecab21a4b32c03645f9dd3668e4547a92d0a6c
matt <matthew.fidler@gmail.com>
2022-09-16 09:00:19 -0500

    Another fix.  More issues still exist

--- R/readModelDb.R
+++ R/readModelDb.R
@@ -3,13 +3,16 @@
 #' @param name The name of the model (must be one of \code{modeldb$name})
 #' @return The model as a function
 #' @export
+#' @examples
+#' readModelDb("PK_1cmt")
 readModelDb <- function(name) {
   if (!(name %in% modeldb$name)) {
     stop("'name' not in database")
   } else {
     .fileName <- modeldb$filename[modeldb$name == name]
     if (!file.exists(.fileName)) {
-      if (file.exists(system(.fileName, package="nlmixr2lib"))) {
+      .fileName <- gsub("inst[/\\]", "", .fileName)
+      if (file.exists(system.file(.fileName, package="nlmixr2lib"))) {
         .fileName <- system.file(.fileName, package="nlmixr2lib")
       }
     }
