d48073be04dbf137e1818f16fa1bc6c47b3e6223
Bill Denney <wdenney@humanpredictions.com>
2022-11-12 17:12:10 -0500

    Find the models in the correct directories

--- tests/testthat/test-readModelDb.R
+++ tests/testthat/test-readModelDb.R
@@ -4,4 +4,8 @@ test_that("readModelDb", {
     regexp = "'name' not in database"
   )
   expect_true(is.function(readModelDb("PK_1cmt")))
+  expect_message(
+    readModelDb("oncology_xenograft_simeoni_2004"),
+    "You can modify the number of damaged cell compartments"
+  )
 })
