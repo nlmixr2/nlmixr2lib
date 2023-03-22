dbb7374487b666303eeeddd7931d7430705583c2
Bill Denney <wdenney@humanpredictions.com>
2023-03-22 11:55:07 -0400

    Update model for most review comments

--- inst/modeldb/PK_2cmt_tvcl_des.R
+++ inst/modeldb/PK_2cmt_tvcl_des.R
@@ -1,26 +1,27 @@
 PK_2cmt_tvcl_des <- function() {
   description <- "Two compartment PK model with time-dependent clearance using differential equations"
+  reference <- "Justin to add"
   ini({
-    lclts <- -1.6 ; label("Time-stationary clearance (CLTS)")
-    ltmax <- -1.5 ; label("Typical value of the maximal change of clearance relative to baseline (Tmax)")
-    lgamma <- 0 ; label("Hill coefficient")
-    lt50 <- 3.4 ; label("Time for 50% of maximal CL change")
-    lvc  <- 3 ; label("Central volume of distribution (V)")
-    lvp  <- 5 ; label("Peripheral volume of distribution (Vp)")
-    lq  <- -0.3 ; label("Intercompartmental clearance (Q)")
+    lcl <- log(0.2) ; label("Time-stationary clearance (CLTS)")
+    lcltmax <- log(0.22) ; label("Typical value of the maximal change of clearance relative to baseline (Tmax)")
+    lclgamma <- log(1) ; label("Hill coefficient for time-dependent clearance")
+    lclt50 <- log(30) ; label("Time for 50% of maximal CL change")
+    lvc  <- log(20) ; label("Central volume of distribution (V)")
+    lvp  <- log(150) ; label("Peripheral volume of distribution (Vp)")
+    lq  <- log(0.75) ; label("Intercompartmental clearance (Q)")
     propSd <- 0.5 ; label("Proportional residual error (fraction)")
   })
   model({
-    clts <- exp(lclts)
-    tmax <- exp(ltmax)
-    gamma <- exp(lgamma)
-    t50 <- exp(lt50)
+    clts <- exp(lcl)
+    cltmax <- exp(lcltmax)
+    clgamma <- exp(lclgamma)
+    clt50 <- exp(lclt50)
     vc <- exp(lvc)
     vp <- exp(lvp)
     q  <- exp(lq)
 
-    cl <- clts*exp(tmax*time^gamma/(t50^gamma+time^gamma))
-    
+    cl <- clts*exp(cltmax*time^clgamma/(clt50^clgamma+time^clgamma))
+
     kel <- cl/vc
     k12 <- q/vc
     k21 <- q/vp
