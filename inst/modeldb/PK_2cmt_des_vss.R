b186ca383c899719f57c7df2ab36eabae1cca615
Kiranmayi Vedantham <kiranmayi.vedantham@gmail.com>
2023-06-07 11:13:28 -0400

    New files of 2cmt and 3cmt with vp renamed to vss and filenames updated

---
+++ inst/modeldb/PK_2cmt_des_vss.R
@@ -0,0 +1,29 @@
+PK_2cmt_des_vss <- function() {
+  description <- "Two compartment PK model with linear clearance using differential equations"
+  ini({
+    lka <- 0.45 ; label("Absorption rate (Ka)")
+    lcl <- 1 ; label("Clearance (CL)")
+    lvc  <- 3 ; label("Central volume of distribution (V)")
+    lvp  <- 5 ; label("Peripheral volume of distribution (Vp)")
+    lq  <- 0.1 ; label("Intercompartmental clearance (Q)")
+    propSd <- 0.5 ; label("Proportional residual error (fraction)")
+  })
+  model({
+    ka <- exp(lka)
+    cl <- exp(lcl)
+    vc <- exp(lvc)
+    vss <- exp(lvp)
+    q  <- exp(lq)
+    
+    kel <- cl/vc
+    k12 <- q/vc
+    k21 <- q/vss
+    
+    d/dt(depot) <- -ka*depot
+    d/dt(central) <-  ka*depot - kel*central - k12*central + k21*peripheral1
+    d/dt(peripheral1) <- k12*central - k21*peripheral1
+    Cc <- central / vc
+    
+    Cc ~ prop(propSd)
+  })
+}
---
+++ inst/modeldb/PK_2cmt_vss.R
@@ -0,0 +1,21 @@
+PK_2cmt_vss <- function() {
+  description <- "Two compartment PK model with linear clearance"
+  ini({
+    lka <- 0.45 ; label("Absorption rate (Ka)")
+    lcl <- 1 ; label("Clearance (CL)")
+    lvc  <- 3 ; label("Central volume of distribution (V)")
+    lvp  <- 5 ; label("Peripheral volume of distribution (Vp)")
+    lq  <- 0.1 ; label("Intercompartmental clearance (Q)")
+    propSd <- 0.5 ; label("Proportional residual error (fraction)")
+  })
+  model({
+    ka <- exp(lka)
+    cl <- exp(lcl)
+    vc <- exp(lvc)
+    vss <- exp(lvp)
+    q  <- exp(lq)
+    
+    Cc<- linCmt()
+    Cc ~ prop(propSd)
+  })
+}
