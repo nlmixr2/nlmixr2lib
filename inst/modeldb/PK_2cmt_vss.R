ba1e0039a7bc98528cc8713c9d9880f798539fc7
Kiranmayi Vedantham <kiranmayi.vedantham@gmail.com>
2023-06-08 12:10:15 -0400

    Included vss <- vp-vc in 2cmt linear models

--- inst/modeldb/PK_2cmt_vss.R
+++ inst/modeldb/PK_2cmt_vss.R
@@ -1,21 +1,21 @@
-PK_2cmt_vss <- function() {
+PK_2cmt_vss   <- function() {
   description <- "Two compartment PK model with linear clearance"
   ini({
-    lka <- 0.45 ; label("Absorption rate (Ka)")
-    lcl <- 1 ; label("Clearance (CL)")
-    lvc  <- 3 ; label("Central volume of distribution (V)")
-    lvp  <- 5 ; label("Peripheral volume of distribution (Vp)")
-    lq  <- 0.1 ; label("Intercompartmental clearance (Q)")
+    lka    <- 0.45 ; label("Absorption rate (Ka)")
+    lcl    <- 1 ; label("Clearance (CL)")
+    lvc    <- 3 ; label("Central volume of distribution (V)")
+    lvss   <- 5 ; label("Volume at steady-state (Vss)")
+    lq     <- 0.1 ; label("Intercompartmental clearance (Q)")
     propSd <- 0.5 ; label("Proportional residual error (fraction)")
   })
   model({
     ka <- exp(lka)
     cl <- exp(lcl)
     vc <- exp(lvc)
-    vss <- exp(lvp)
+    vss<- exp(lvss)
     q  <- exp(lq)
     
-    Cc<- linCmt()
+    Cc <- linCmt()
     Cc ~ prop(propSd)
   })
 }
