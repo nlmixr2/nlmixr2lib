18bcc9f07d939064c16447fc7cf730e7f092a258
Kiranmayi Vedantham <kiranmayi.vedantham@gmail.com>
2023-06-13 11:09:55 -0400

    PK compartment models with Michealis-Menten elimination kinetics

--- inst/modeldb/PK_1cmt_mm.R
+++ inst/modeldb/PK_1cmt_mm.R
@@ -3,17 +3,18 @@ PK_1cmt_mm <- function() {
   ini({
     lka <- 0.45 ; label("Absorption rate (Ka)")
     lvm <- 0.04; label("maximum target-mediated rate of elimination (mg/L/d)")
-    Km <- 0.01; label("Michaelis-Menten constant (mg/L)")
-    lvc  <- 3.45 ; label("Central volume of distribution (V)")
+    lkm <- 0.01; label("Michaelis-Menten constant (mg/L)")
+    lvc  <- 3.45 ; label("Central volume of distribution (Vc)")
     propSd <- 0.5 ; label("Proportional residual error (fraction)")
   })
   model({
     ka <- exp(lka)
-    Vm <- exp(lvm)
+    vm <- exp(lvm)
+    km <- exp (lkm)
     vc  <- exp(lvc)
     
     d/dt(depot) <- -ka*depot
-    d/dt(central) <- ka*depot-central*(Vm/(Km + central/vc))
+    d/dt(central) <- ka*depot-central*(vm/(km + central/vc))
     
     Cc <- central / vc
     Cc ~ prop(propSd)
