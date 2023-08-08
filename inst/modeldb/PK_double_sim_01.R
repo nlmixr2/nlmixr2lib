a38482d6f65ee5f2155605026b03fecbe4a6f3b4
Kiranmayi Vedantham <kiranmayi.vedantham@gmail.com>
2023-08-08 13:13:45 -0400

    PK double absorption models

--- inst/modeldb/PK_double_sim_01.R
+++ inst/modeldb/PK_double_sim_01.R
@@ -6,17 +6,24 @@ PK_double_sim_01 <- function() {
     lcl <- 1 ; label("Clearance (CL)")
     lvc  <- 3 ; label("Central volume of distribution (V)")
     propSd <- 0.5 ; label("Proportional residual error (fraction)")
+    lgfdepot1 <- logit(0.8);
+    lalag <- log (9); 
   })
   model({
     k01 <- exp(tk01)
     ka2 <- exp(lka2)
     cl <- exp(lcl)
     vc <- exp(lvc)
+    fdepot1 <- expit(lgfdepot1)
+    alag <- exp(lalag)
     
     kel <- cl/vc
     
     d/dt(depot1) <- -k01
+    f(depot1) <- fdepot1
     d/dt(depot2) <- -ka2*depot2
+    lag(depot2) <- alag
+    f(depot2) <- 1-fdepot1
     d/dt(central) <-  k01+ka2*depot2-kel*central 
     
     Cc <- central / vc
--- inst/modeldb/PK_double_sim_11.R
+++ inst/modeldb/PK_double_sim_11.R
@@ -6,21 +6,31 @@ PK_double_sim_11 <- function() {
     lcl <- 1 ; label("Clearance (CL)")
     lvc  <- 3 ; label("Central volume of distribution (V)")
     propSd <- 0.5 ; label("Proportional residual error (fraction)")
+    lgfdepot1 <- logit(0.7);
+    lalag <- log (9); 
   })
   model({
     ka1 <- exp(lka1)
     ka2 <- exp(lka2)
     cl <- exp(lcl)
     vc <- exp(lvc)
+    fdepot1 <- expit(lgfdepot1)
+    alag <- exp(lalag)
     
     kel <- cl/vc
     
     d/dt(depot1) <- -ka1*depot1
+    f(depot1) <- fdepot1
     d/dt(depot2) <- -ka2*depot2
-    d/dt(central) <-  ka1*depot1+ka2*depot2 -kel*central 
+    lag(depot2) <- alag
+    f(depot2) <- 1-fdepot1
+    d/dt(central) <-  ka1*depot1+ka2*depot2 - kel*central 
     
     Cc <- central / vc
     
     Cc ~ prop(propSd)
   })
 }
+
+
+
