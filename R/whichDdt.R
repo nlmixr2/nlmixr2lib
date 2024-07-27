d22413cfe3b1dcd1adad5964f35ed49b14de9683
Matthew L. Fidler <matthew.fidler@gmail.com>
2024-07-26 23:20:23 -0500

    Break out whichDdt

---
+++ R/whichDdt.R
@@ -0,0 +1,32 @@
+#' Figures out what line d/dt(central) is on
+#'
+#' @param modelLines modelLines expression list
+#' @param central name of central compartment
+#' @param ddt is this a derivative expression
+#' @return which item in modelLines is the central compartment (or
+#'   error if there is multiple lines)
+#' @noRd
+#' @author Matthew L. Fidler
+.whichDdt <- function(modelLines, central, ddt=TRUE) {
+  .dd1 <- ifelse(ddt, "d/dt(", "")
+  .dd2 <- ifelse(ddt, ")", "")
+  .ddtCentral1 <- str2lang(paste0(.dd1, central, .dd2, " <- ."))
+  .ddtCentral2 <- str2lang(paste0(.dd1, central, .dd2, " = ."))
+  .w <- which(vapply(seq_along(modelLines),
+                     function(i) {
+                       .cur <- modelLines[[i]]
+                       rxode2::.matchesLangTemplate(.cur, .ddtCentral1) ||
+                         rxode2::.matchesLangTemplate(.cur, .ddtCentral2)
+                     }, logical(1), USE.NAMES = FALSE))
+  # Modify ODE for central compartment
+  if (length(.w) != 1) {
+    stop("'",
+         .dd1,
+         central, ifelse(ddt, ")", ""),
+         "'",
+         .dd2,
+         "' not found in model",
+         call.=FALSE)
+  }
+  .w
+}
