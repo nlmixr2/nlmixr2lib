40bdc27632a43c3bba905164ab3752cd54da7ffe
Matthew L. Fidler <matthew.fidler@gmail.com>
2024-07-31 16:24:42 -0500

    Add Weibull absorption transit compartment tests

--- tests/testthat/test-addWeibullAbs.R
+++ tests/testthat/test-addWeibullAbs.R
@@ -41,6 +41,28 @@ test_that("Test addWeibullAbs function adds depot", {
 
 })
 
+test_that("Test addWeibullAbs function removes transit", {
+
+  ui <- rxode2::rxode2(readModelDb("PK_1cmt_des")) %>% addTransit(3)
+
+  expect_warning(addWeibullAbs(ui))
+  result <- suppressWarnings(addWeibullAbs(ui))
+
+  expect_s3_class(result, "rxUi")
+
+  expect_equal(rxode2::modelExtract(result, "d/dt(depot)"),
+               "d/dt(depot) <- -(wb/wa) * (tad0(depot)/wa)^(wb - 1) * depot")
+
+  expect_equal(rxode2::modelExtract(result, "d/dt(depot)"),
+               "d/dt(depot) <- -(wb/wa) * (tad0(depot)/wa)^(wb - 1) * depot")
+  expect_equal(rxode2::modelExtract(result, "d/dt(central)"),
+               "d/dt(central) <- (wb/wa) * (tad0(depot)/wa)^(wb - 1) * depot - kel * central")
+  expect_false("lka" %in% names(result$theta))
+  expect_true("lwa" %in% names(result$theta))
+  expect_true("lwb" %in% names(result$theta))
+
+})
+
 test_that("Test addWeibullAbs function with missing ui", {
   expect_error(addWeibullAbs())
 })
