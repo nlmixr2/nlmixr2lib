864caeecc6d50677bb0c45fbc32229214e572b30
Matthew Fidler <matthew.fidler@gmail.com>
2024-08-21 18:13:52 -0500

    Fix test

--- tests/testthat/test-transPk.R
+++ tests/testthat/test-transPk.R
@@ -5,7 +5,7 @@ test_that("transPk -- k", {
   expect_true(all(c("lkel", "lk12", "lk21", "lk13", "lk31") %in% names(p3k$theta)))
 
   # Issue #87 -- Make sure depot is not dropped
-  expect_equal(rxode2::modelExtract(pk3, "d/dt(depot)"),
+  expect_equal(rxode2::modelExtract(p3k, "d/dt(depot)"),
                "d/dt(depot) <- -ka * depot")
 
   p2k <- readModelDb("PK_2cmt_des") |>
