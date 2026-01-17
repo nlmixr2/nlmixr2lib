5685ae6067c7bd897923201fd147cf8ea6228dd3
Bill Denney <wdenney@humanpredictions.com>
2026-01-17 16:00:28 -0500

    Move `removeTransit()` testing to match its R/ file location

--- tests/testthat/test-addTransit.R
+++ tests/testthat/test-addTransit.R
@@ -1,3 +1,5 @@
+# Add transit ----
+
 test_that("addTransit adds transit compartment", {
   modelTest <- readModelDb("PK_1cmt_des") |> addTransit(3)
   modelTest <- rxode2::assertRxUi(modelTest)
@@ -11,7 +13,6 @@ test_that("addTransit adds transit compartment", {
   suppressMessages(kaLine <- rxode2::modelExtract(modelUpdate, "ktr", lines = TRUE))
   expect_equal(grepl("\\s*^ktr", kaLine), TRUE)
 })
-
 
 # Test if the function throws an error when invalid input for 'central' is provided
 test_that("addTransit throws an error with invalid 'central'", {
@@ -78,4 +79,163 @@ test_that("extreme model cases", {
 
   expect_equal(omega, tmp$omega)
 
+})
+
+# Reemove transit ----
+
+test_that("removeTransit throws error in model with no transit compartment", {
+  expect_error(removeTransit(readModelDb("PK_2cmt_des"), "central", transit = "transit"), "Assertion on 'ntransit' failed: Must be of type 'integerish', not 'character'")
+})
+test_that("removeTransit removes transit compartment", {
+  modelTest <- readModelDb("PK_1cmt_des") |> addTransit(1)
+  modelTest <- rxode2::assertRxUi(modelTest)
+  suppressMessages(modelUpdate <- removeTransit(modelTest, central = "central", depot = "depot", transit = "transit"))
+  temp <- rxode2::assertRxUi(modelUpdate)
+  mv <- rxode2::rxModelVars(temp)
+  expect_equal("transit1" %in% mv$state, FALSE)
+})
+test_that("removeTransit removes ktr in model block", {
+  modelTest <- readModelDb("PK_1cmt_des") |> addTransit(1)
+  modelTest <- rxode2::assertRxUi(modelTest)
+  suppressMessages(modelUpdate <- removeTransit(modelTest, central = "central", depot = "depot", transit = "transit"))
+  temp <- rxode2::assertRxUi(modelUpdate)
+  mv <- rxode2::rxModelVars(temp)
+  expect_equal("ktr" %in% mv$lhs, FALSE)
+})
+test_that("removeTransit modifies ODE for central compartment", {
+  modelTest <- readModelDb("PK_1cmt_des") |> addTransit(1)
+  modelTest <- rxode2::assertRxUi(modelTest)
+  suppressMessages(modelUpdate <- removeTransit(modelTest, central = "central", depot = "depot"))
+  suppressMessages(centralLine <- rxode2::modelExtract(modelUpdate, "d/dt(central)", lines = TRUE))
+  expect_equal(grepl(".*<-\\s*", centralLine), TRUE)
+})
+test_that("removeTransit removes lktr in ini block", {
+  modelTest <- readModelDb("PK_1cmt_des") |> addTransit(1)
+  modelTest <- rxode2::assertRxUi(modelTest)
+  suppressMessages(modelUpdate <- removeTransit(modelTest, central = "central", depot = "depot"))
+  temp <- rxode2::assertRxUi(modelUpdate)
+  mv <- rxode2::rxModelVars(temp)
+  expect_equal("lktr1" %in% mv$params, FALSE)
+})
+
+
+test_that("extreme model cases", {
+
+  f <- function() {
+    model({
+      ktr <- exp(lktr)
+      d/dt(depot) <- -ktr * depot
+      d/dt(transit1) <- ktr * depot - ktr * transit1
+      d/dt(transit2) <- ktr * transit1 - ktr * transit2
+      d/dt(transit3) <- ktr * transit2 - ktr * transit3
+      d/dt(transit4) <- ktr * transit3 - ka * transit4
+      d/dt(central) <- ka * transit4 - kel * central
+      Cc <- central/vc
+    })
+  }
+
+  f <- rxode2::rxode2(f)
+
+  expect_error(f %>% removeTransit(), NA)
+  expect_warning(f %>% removeTransit(), NA)
+  tmp <- f %>% removeTransit()
+  expect_true(length(tmp$iniDf$name) == 0)
+
+  f <- function() {
+    ini({
+      lktr <- 1
+    })
+    model({
+      ktr <- exp(lktr)
+      d/dt(depot) <- -ktr * depot
+      d/dt(transit1) <- ktr * depot - ktr * transit1
+      d/dt(transit2) <- ktr * transit1 - ktr * transit2
+      d/dt(transit3) <- ktr * transit2 - ktr * transit3
+      d/dt(transit4) <- ktr * transit3 - ka * transit4
+      d/dt(central) <- ka * transit4 - kel * central
+      Cc <- central/vc
+    })
+  }
+
+  f <- rxode2::rxode2(f)
+
+  expect_error(f %>% removeTransit(), NA)
+  expect_warning(f %>% removeTransit(), NA)
+  tmp <- f %>% removeTransit()
+  expect_true(length(tmp$iniDf$name) == 0)
+
+  f <- function() {
+    ini({
+      lktr ~ 1
+    })
+    model({
+      ktr <- exp(lktr)
+      d/dt(depot) <- -ktr * depot
+      d/dt(transit1) <- ktr * depot - ktr * transit1
+      d/dt(transit2) <- ktr * transit1 - ktr * transit2
+      d/dt(transit3) <- ktr * transit2 - ktr * transit3
+      d/dt(transit4) <- ktr * transit3 - ka * transit4
+      d/dt(central) <- ka * transit4 - kel * central
+      Cc <- central/vc
+    })
+  }
+
+  f <- rxode2::rxode2(f)
+
+  expect_error(f %>% removeTransit(), NA)
+  expect_warning(f %>% removeTransit(), NA)
+  tmp <- f %>% removeTransit()
+  expect_true(length(tmp$iniDf$name) == 0)
+
+  f <- function() {
+    ini({
+      ka <- 1
+      lktr ~ 1
+    })
+    model({
+      ktr <- exp(lktr)
+      d/dt(depot) <- -ktr * depot
+      d/dt(transit1) <- ktr * depot - ktr * transit1
+      d/dt(transit2) <- ktr * transit1 - ktr * transit2
+      d/dt(transit3) <- ktr * transit2 - ktr * transit3
+      d/dt(transit4) <- ktr * transit3 - ka * transit4
+      d/dt(central) <- ka * transit4 - kel * central
+      Cc <- central/vc
+    })
+  }
+
+  f <- rxode2::rxode2(f)
+
+  expect_error(f %>% removeTransit(), NA)
+
+  tmp <- f %>% removeTransit()
+
+  expect_equal(tmp$iniDf$name, "ka")
+  expect_equal(tmp$iniDf$ntheta, 1)
+
+  f <- function() {
+    ini({
+      ka ~ 1
+      lktr ~ 1
+    })
+    model({
+      ktr <- exp(lktr)
+      d/dt(depot) <- -ktr * depot
+      d/dt(transit1) <- ktr * depot - ktr * transit1
+      d/dt(transit2) <- ktr * transit1 - ktr * transit2
+      d/dt(transit3) <- ktr * transit2 - ktr * transit3
+      d/dt(transit4) <- ktr * transit3 - ka * transit4
+      d/dt(central) <- ka * transit4 - kel * central
+      Cc <- central/vc
+    })
+  }
+
+  f <- rxode2::rxode2(f)
+
+  expect_error(f %>% removeTransit(), NA)
+
+  tmp <- f %>% removeTransit()
+
+  expect_equal(tmp$iniDf$name, "ka")
+  expect_equal(tmp$iniDf$neta1, 1)
 })
